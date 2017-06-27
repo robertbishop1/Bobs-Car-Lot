4675366f6b635015214f4d41c5f46f134fc7f052
Robert Bishop <robert.bishop001@mymdc.net>
2017-06-26 21:09:25 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ void mainMenu() {
     std::cout << "Welcome to Bob's used car lot and chop shop!\n" << "As you "
             "can see we have quite a few to choose from!\n" << "Which one "
                       "would you like?:" << std::endl;
+
+    std::cout << "[1] 2005 Volkswagen Beetle ($8,000)";
 }
 
 void carMenu() {
