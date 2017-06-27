fb72280c50dc21e601279e9697c47960baac81f6
Robert Bishop <robert.bishop001@mymdc.net>
2017-06-26 20:59:11 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 #include <string>
 
 void mainMenu() {
+    std::cout << "Welcome to Bob's used car lot and chop shop!\n" << "As you "
+            "can see we have quite a few to choose from!\n" << "Which one "
+                      "would you like?:" << std::endl;
+}
+
+void carMenu() {
+
 }
 
 int main() {
-    std::cout << "Welcome to Bob's used car lot and chop shop!\n" << "As you "
-            "can see we have quite a few to choose from!\n" << "Which one "
-            "would you like?:" << std::endl;
-
+    int carLevel, quoteNumber;
+    double carCost;
+    mainMenu();
     return 0;
 }
