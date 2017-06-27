f2a5dee709996ca9992a0f20efb147e6e42fc968
Robert Bishop <robert.bishop001@mymdc.net>
2017-06-26 20:48:30 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to Bob's used car lot and chop shop!\n" << "As you "
+            "can see we have quite a few to choose from!\n" << "Which one "
+            "would you like?:" << std::endl;
+
+    return 0;
+}
