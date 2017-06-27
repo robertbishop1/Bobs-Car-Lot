121231885754435d34e691539e207594ea2d3e7d
Robert Bishop <robert.bishop001@mymdc.net>
2017-06-26 20:52:54 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <iomanip>
 #include <string>
 
+void mainMenu() {
+}
+
 int main() {
     std::cout << "Welcome to Bob's used car lot and chop shop!\n" << "As you "
             "can see we have quite a few to choose from!\n" << "Which one "
