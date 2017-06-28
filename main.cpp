80a1a95e941f9653d9c8e946e40c070aaca964c1
Robert Bishop <robert.bishop001@mymdc.net>
2017-06-27 23:28:41 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <string>
 
+// This will be the main menu and also the menu shown upon a reset
 void mainMenu() {
     std::cout << "Welcome to Bob's used car lot and chop shop!\n" << "As you "
             "can see we have quite a few to choose from!\n" << "Which one "
@@ -10,6 +11,7 @@ void mainMenu() {
     std::cout << "[1] 2005 Volkswagen Beetle ($8,000)";
 }
 
+// This menu will show you car quote, with price and quantity of upgrades
 void carMenu() {
 
 }
