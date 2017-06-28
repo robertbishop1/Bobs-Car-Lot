bf3791ad735cd288e1a63e2d686ec4bdeeef8cf8
Robert Bishop <robert.bishop001@mymdc.net>
2017-06-27 23:50:27 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,30 @@
 #include <iomanip>
 #include <string>
 
-// This will be the main menu and also the menu shown upon a reset
+const int VWBEET = 8000;
+const int PONTG6 = 8581;
+const int CHEVS10 = 10500;
+const int JPPAT = 15209;
+const int JPWRANG = 24390;
+
+// This menu shows the value of current car including upgrades
+int carMenu(int) {
+    
+}
+
 void mainMenu() {
     std::cout << "Welcome to Bob's used car lot and chop shop!\n" << "As you "
             "can see we have quite a few to choose from!\n" << "Which one "
                       "would you like?:" << std::endl;
 
-    std::cout << "[1] 2005 Volkswagen Beetle ($8,000)";
+    std::cout << "[1] 2005 Volkswagen Beetle ($8,000)\n" <<
+              "[2] 2008 Pontiac G6 ($8,581)\n" <<
+              "[3] 2004 Chevy S-10 ($10,500)\n" <<
+              "[4] 2016 Jeep Patriot ($15,209)" <<
+              "[5] 2012 Jeep Wrangler Sport ($24,390)";
+    cin >> carMenu();
 }
 
-// This menu will show you car quote, with price and quantity of upgrades
-void carMenu() {
-
-}
 
 int main() {
     int carLevel, quoteNumber;
