3c5a9b4edffd27f31856166651a5eda84de1c446
Robert Bishop <robert.bishop001@mymdc.net>
2017-06-28 15:08:25 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,34 +2,30 @@
 #include <iomanip>
 #include <string>
 
-// These are the constants for the vehicle prices
 const int VWBEET = 8000;
 const int PONTG6 = 8581;
 const int CHEVS10 = 10500;
 const int JPPAT = 15209;
 const int JPWRANG = 24390;
 
-//This holds the current selected vehicle
-int currentVeh;
-// This holds the current quote number
-int quoteNum;
-// This holds the current amount of upgrades placed on the vehicle this is not the value
-int numUpgrades;
+
 
 // This menu shows the value of current car including upgrades
-int carMenu(int) {
-
+int vehicleMenu(int currentVeh) {
+   int quoteNum;
+   int vehicleValue;
    switch (currentVeh) {
-       case 1: currentVeh = 8000;
+       case 1: vehicleValue = 8000;
            break;
    }
 
-    std::cout << "Quote:{" << quoteNum << "}  Car($" << currentVeh <<
+    std::cout << "Quote:{" << quoteNum << "}  Car($" << vehicleValue <<
               ")";
 
 }
 
 void mainMenu() {
+    int currentVeh;
     std::cout << "Welcome to Bob's used car lot and chop shop!\n" << "As you "
             "can see we have quite a few to choose from!\n" << "Which one "
                       "would you like?:" << std::endl;
@@ -41,13 +37,11 @@ void mainMenu() {
               "[5] 2012 Jeep Wrangler Sport ($24,390)\n" <<
               "Which car would you like?:";
     std::cin >> currentVeh;
-    carMenu(currentVeh);
+    vehicleMenu(currentVeh);
 }
 
 
 int main() {
-    int carLevel, quoteNumber;
-    double carCost;
     mainMenu();
     return 0;
 }
